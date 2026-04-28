7250443a957efd34809c9e6bf3ff106f9a4bf1e4
SALIKHASKIEH <sallykh04@gmail.com>
2026-04-29 02:09:20 +0300

    Add CircularBuffer implementation in Assignment1.c
    Implement a circular buffer with read and write operations.

---
+++ Assignment1.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE 50
+
+struct CircularBuffer {
+    char buffer[SIZE];
+    int head;
+    int tail;
+    int count;
+};
+
+void init(struct CircularBuffer *cb) {
+    cb->head = 0;
+    cb->tail = 0;
+    cb->count = 0;
+}
+
+int isFull(struct CircularBuffer *cb) {
+    return cb->count == SIZE;
+}
+
+int isEmpty(struct CircularBuffer *cb) {
+    return cb->count == 0;
+}
+
+void write(struct CircularBuffer *cb, char data) {
+    if (isFull(cb)) {
+        printf("Buffer Overflow\n");
+        return;
+    }
+    cb->buffer[cb->head] = data;
+    cb->head = (cb->head + 1) % SIZE;
+    cb->count++;
+}
+
+char read(struct CircularBuffer *cb) {
+    if (isEmpty(cb)) {
+        printf("Buffer Underflow\n");
+        return ;
+    }
+    char data = cb->buffer[cb->tail];
+    cb->tail = (cb->tail + 1) % SIZE;
+    cb->count--;
+    return data;
+}
+
+int main() {
+    struct CircularBuffer cb;
+    init(&cb);
+
+    char name[50];
+    printf("Enter name: ");
+    scanf("%s", name);
+    strcat(name, "CE-ESY");
+
+    for (int i = 0; i < strlen(name); i++) {
+        write(&cb, name[i]);
+    }
+    while (!isEmpty(&cb)) {
+        char ch = read(&cb);
+        printf("%c", ch);
+    }
+    printf("\n");
+
+    return 0;
+}
